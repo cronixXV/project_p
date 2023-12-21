dee9422f686321f906b6075fee18ec680204febe
Egor <egruzdev14gmail.com>
2023-12-21 22:10:34 +0300

    fourth step

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const { TRUE } = require("sass");
+
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
